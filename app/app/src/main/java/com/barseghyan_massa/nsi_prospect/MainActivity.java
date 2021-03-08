c17743b83e7964d14b90aecf23a8fb29dd4ca71c
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2021-03-08 17:49:15 +0100

    debu connexion -  11:18
    https://www.youtube.com/watch?v=lF5m4o_CuNg

--- app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
+++ app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
@@ -2,13 +2,38 @@ package com.barseghyan_massa.nsi_prospect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inputLogin;
+    private EditText inputPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputLogin = (EditText) findViewById(R.id.text_login);
+        inputPassword = (EditText) findViewById(R.id.text_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+    }
+
+    public void validate(String username, String password) {
+        if((username == "Admin") && (password == "1234")){
+            Intent intent = new Intent(MainActivity.this, HomepageActivity.class);
+        }
+    }
+
+    /*
+    Display toast
+     */
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_connected, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
