5243855d61bd96e0479844f0cd6fddf35335482f
achot-barseghyan <57489459+achot-barseghyan@users.noreply.github.com>
2021-03-12 17:47:54 +0100

    update

--- app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
+++ app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
@@ -2,7 +2,13 @@ package com.barseghyan_massa.nsi_prospect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //BUTTONS
+        Button option_btn;
+
+        //OPEN Drawer
+        option_btn = findViewById(R.id.login_btn);
+        option_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHomepage();
+            }
+        });
+
+    }
+
+    public void goToHomepage() {
+        Intent homepage = new Intent(MainActivity.this, HomepageActivity.class);
+        startActivity(homepage);
     }
 }
