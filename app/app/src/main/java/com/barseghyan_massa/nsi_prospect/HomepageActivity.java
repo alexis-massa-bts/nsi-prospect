78b1c70ac00a3402fa73a93cfad983c3ca6579a8
achot-barseghyan <57489459+achot-barseghyan@users.noreply.github.com>
2021-03-15 17:34:45 +0100

    update

--- app/app/src/main/java/com/barseghyan_massa/nsi_prospect/HomepageActivity.java
+++ app/app/src/main/java/com/barseghyan_massa/nsi_prospect/HomepageActivity.java
@@ -1,15 +1,64 @@
 package com.barseghyan_massa.nsi_prospect;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
 public class HomepageActivity extends AppCompatActivity {
 
+    //References
+    ImageView logout_btn;
+    MaterialSpinner spinner_event;
+    MaterialSpinner spinner_company;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        //Declarations
+        logout_btn = findViewById(R.id.logout);
+        spinner_event = (MaterialSpinner) findViewById(R.id.event);
+        spinner_company = (MaterialSpinner) findViewById(R.id.company);
+
+        //Buttons listners
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Successfull logout", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        //Fill spinners
+        spinner_company.setItems("item 1", "item 2", "item 3", "item 4", "item 5");
+        spinner_event.setItems("item 1", "item 2", "item 3", "item 4", "item 5");
+
+        //Spinners events
+        spinner_event.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        spinner_company.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
--- app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
+++ app/app/src/main/java/com/barseghyan_massa/nsi_prospect/MainActivity.java
@@ -15,7 +15,7 @@ import com.barseghyan_massa.nsi_prospect.db.model.Prospect;
 public class MainActivity extends AppCompatActivity {
 
     //References
-    Button option_btn;
+    Button login_btn;
     EditText et_name, et_password;
 
     @Override
@@ -23,31 +23,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //Declarations
-        option_btn = findViewById(R.id.login_btn);
+        login_btn = findViewById(R.id.login_btn);
         et_name = findViewById(R.id.text_name);
         et_password = findViewById(R.id.text_password);
 
         //Button listeners
-        option_btn.setOnClickListener(new View.OnClickListener() {
+        login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Test création d'un Prospect pour test connexion BDD
-                Prospect prospect;
-                try {
-                    prospect = new Prospect("john", "doe");
-                    Toast.makeText(MainActivity.this, prospect.toString(), Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    prospect = new Prospect();
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
-
-                DatabaseHelper dbHelper = new DatabaseHelper(MainActivity.this);
-                boolean success = dbHelper.addOne(prospect);
-                Toast.makeText(MainActivity.this, "Success" + success, Toast.LENGTH_SHORT).show();
-
-//                goToHomepage();
+                goToHomepage();
             }
         });
 
