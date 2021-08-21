18be3499c6a6e7bc1c9243b9324fdca37bbf791b
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2021-08-21 22:47:07 +0200

    Removed sensitive informations

--- app/app/src/main/java/com/barseghyan_massa/nsi_prospect/mail/Config.java
+++ app/app/src/main/java/com/barseghyan_massa/nsi_prospect/mail/Config.java
@@ -2,6 +2,6 @@ package com.barseghyan_massa.nsi_prospect.mail;
 
 public class Config {
 
-    public static final String EMAIL = "alexis2massa@gmail.com";
-    public static final String PASSWORD = "P@ssw0rdSIO";
+    public static final String EMAIL = "";
+    public static final String PASSWORD = "";
 }
